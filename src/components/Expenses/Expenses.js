f152ef2144873af34a03df8356662d514e37f254
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2021-12-22 23:10:46 +0500

    ExpenseFilter Component - Stateless or Controlled Component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,32 @@
-import React from 'react'
-import { expenses } from '../../data/expenses'
-import ExpenseItem from './ExpenseItem'
-import Card from '../UI/Card'
-import './Expenses.css'
+import React, { useState } from "react";
+import { expenses } from "../../data/expenses";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpenseFilter from "./ExpenseFilter";
 export default function Expenses() {
-    return (
-        <Card className="expenses">
-   { expenses.map((value,key) => {
-      return <ExpenseItem key={key} expenseDate={value.date} expenseTitle={value.title} expenseAmount={value.amount}/>
-    })      
-   } 
-     </Card>
-    )
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenses.map((value, key) => {
+          return (
+            <ExpenseItem
+              key={key}
+              expenseDate={value.date}
+              expenseTitle={value.title}
+              expenseAmount={value.amount}
+            />
+          );
+        })}
+      </Card>
+    </div>
+  );
 }
