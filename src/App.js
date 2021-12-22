f26bcfc222ddde113cbb9bfa2111bf3db79eee72
AbdullahGhani1 <engr.abdullahghani@gmail.com>
2021-12-22 18:56:25 +0500

    Two way dataBinding Parent-to-child or child-to-parent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
+import "./App.css";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+// import { expenses } from "./data/expenses";
 
 function App() {
-  return <div>
-    <NewExpense/>
-    <Expenses /> 
-  </div>
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import './NewExpense.css'
-import ExpenseForm from './ExpenseForm'
-import Card from '../UI/Card'
-export default function NewExpense() {
-    return (
-        <Card className="new-expense">
-       <ExpenseForm/>
-        </Card>
-    )
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+import Card from "../UI/Card";
+export default function NewExpense(props) {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <Card className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </Card>
+  );
 }
